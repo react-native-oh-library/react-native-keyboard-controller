25c9fc0b23664aaca586116173755dc79ed9eef1
hewanglong <3132734221@qq.com>
2026-05-07 09:41:43 +0800

    fix:解决监听输入框文本变化失效的问题 (#73)
    Signed-off-by: hewanglong <hewanglong1@h-partners.com>
    Co-authored-by: hewanglong <hewanglong1@h-partners.com>

--- harmony/keyboard_controller/src/main/cpp/KeyboardControllerViewComponentInstance.cpp
+++ harmony/keyboard_controller/src/main/cpp/KeyboardControllerViewComponentInstance.cpp
@@ -191,6 +191,11 @@ void KeyboardControllerViewComponentInstance::onChange(std::string text) {
      }
 
 }
+
+void KeyboardControllerViewComponentInstance::onChange(std::string text, std::string extendStr) {
+    onChange(std::move(text));
+}
+
 void KeyboardControllerViewComponentInstance::onTextSelectionChange(int32_t location, int32_t length) {
     DLOG(INFO) << " onKeyboardControllerView onTextSelectionChange";
     // to do
